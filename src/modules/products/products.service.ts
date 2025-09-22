6722b8d8768a4a14672a9195ffa0afc6734a832d
Tanvir286 <tanvirdiu200@gmail.com>
2025-09-22 17:44:10 +0600

    add product with get

--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -1,4 +1,4 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { Condition, CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
@@ -38,7 +38,7 @@ export class ProductsService {
       data: {
         product_title,
         product_description,
-        price,
+        price:1,
         stock,
         photos,
         location,
@@ -65,16 +65,64 @@ export class ProductsService {
     }
   }
 
-  // get all products
+  // Get all products
+  async findAll() {
+    const products = await this.prisma.product.findMany({
+      include: {
+        category: true, 
+      },
+    });
 
-  findAll() {
-    return `This action returns all products`;
+    // Check if no products are found
+    if (products.length === 0) {
+      throw new Error('No products found');
+    }
+
+    return {
+      message: 'Products retrieved successfully',
+      data: products.map(product => ({
+        id: product.id,
+        product_title: product.product_title,
+        product_description: product.product_description,
+        price: product.price,
+        stock: product.stock,
+        category: {
+          id: product.category.id,
+          category_name: product.category.category_name,
+        }
+      })),
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  // Get a product by ID
+  async findOne(id: string) {
+
+    const product = await this.prisma.product.findUnique({
+      where: { id },
+      include: { category: true },
+    });
+    
+    if (!product) {
+      throw new NotFoundException(`Product with ID ${id} not found`);
+    }
+
+    return {
+      message: 'Product retrieved successfully',
+      data: {
+        id: product.id,
+        product_title: product.product_title, 
+        product_description: product.product_description,
+        price: product.price,
+        stock: product.stock,
+        category: {
+          id: product.category.id,
+          category_name: product.category.category_name,
+        },
+      },
+    };
   }
 
+  // update by product
   update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
