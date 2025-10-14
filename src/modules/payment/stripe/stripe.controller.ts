142203f744f97be02a0601cbb57a6ce1c7ec39af
Tanvir286 <tanvirdiu200@gmail.com>
2025-10-14 17:17:51 +0600

    payment

--- src/modules/payment/stripe/stripe.controller.ts
+++ src/modules/payment/stripe/stripe.controller.ts
@@ -5,6 +5,8 @@ import {
   Headers,
   UseGuards,
   Body,
+  Param,
+  NotFoundException,
 } from '@nestjs/common';
 import { StripeService } from './stripe.service';
 import { Request } from 'express';
@@ -24,142 +26,173 @@ export class StripeController {
     private readonly orderService: OrderService,
   ) {}
 
-  // @Post('pay')
-  // @UseGuards(JwtAuthGuard)
-  // async pay(@Req() req: any, @Body() body: CreateOrderDto) {
-  //   try {
-  //     const buyer_id = req.user.userId;
-  //     // here i want to create order after payment is successful
-  //     const result = await this.orderService.placeOrder(
-  //       buyer_id,
-  //       body.seller_id,
-  //       body.shipping_info,
-  //       body.order_products,
-  //     );
-  //     const customer = await this.prisma.user.findUnique({
-  //       where: { id: result.buyer_id },
-  //     });
+  @Post('pay/:seller_id')
+  @UseGuards(JwtAuthGuard)
+  async pay(
+    @Req() req: any, 
+    @Param('seller_id') seller_id: string,
+    @Body() dto: CreateOrderDto) {
+    try {
+      const buyer_id = req.user.userId;
 
-  //     const payment = await StripePayment.createPaymentIntent({
-  //       customer_id: customer.billing_id,
-  //       amount: Number(result.grand_total),
-  //       currency: 'usd',
-  //       metadata: {
-  //         order_name: `Order_${result.id}`,
-  //         order_id: result.id,
-  //         buyer_id: result.buyer_id,
-  //         seller_id: result.seller_id,
-  //         total_pay: Number(result.grand_total),
-  //       },
-  //     });
+     // 🟢 1. Create Order first → status = PENDING
+      const orderResponse = await this.orderService.createOrder(
+        buyer_id,
+        seller_id,
+        dto
+      );
 
-  //     console.log('PaymentIntent Created:', payment.client_secret);
-  //     console.log('Metadata:', payment.metadata);
+       if (!orderResponse.success) {
+        throw new Error(orderResponse.message);
+      }
 
-  //     return {
-  //       clientSecret: payment.client_secret,
-  //       message: 'PaymentIntent created successfully',
-  //       totalAmount: Number(result.grand_total),
-  //     };
-  //   } catch (error) {
-  //     return {
-  //       success: false,
-  //       message: `Error placing order : ${error.message}`,
-  //     };
-  //   }
-  // }
+      const orderId = orderResponse.data.order_id;
+      const totalAmount = Number(orderResponse.data.total);
 
-  // @Post('webhook')
-  // async handleWebhook(
-  //   @Headers('stripe-signature') signature: string,
-  //   @Req() req: Request,
-  // ) {
-  //   try {
-  //     const payload = req.rawBody.toString();
-  //     const event = await this.stripeService.handleWebhook(payload, signature);
+      const customer = await this.prisma.user.findUnique({
+        where: { id: buyer_id },
+      });
 
-  //     // Handle events
-  //     switch (event.type) {
-  //       case 'customer.created':
-  //         break;
-  //       case 'payment_intent.created':
-  //         break;
-  //       case 'payment_intent.succeeded':
-  //         const paymentIntent = event.data.object;
-  //         const successMetadata = paymentIntent.metadata;
-  //         // create tax transaction
-  //         // await StripePayment.createTaxTransaction(
-  //         //   paymentIntent.metadata['tax_calculation'],
-  //         // );
-  //         // Update transaction status in database
 
-  //         // here update order status to paid
-  //         await this.prisma.order.update({
-  //           where: { id: successMetadata.order_id },
-  //           data: { payment_status: PaymentStatus.PAID },
-  //         });
+      if (!customer || !customer.billing_id) {
+        throw new NotFoundException('Customer not found or missing billing ID');
+      }
 
-  //         await TransactionRepository.updateTransaction({
-  //           reference_number: paymentIntent.id,
-  //           status: 'succeeded',
-  //           paid_amount: paymentIntent.amount / 100, // amount in dollars
-  //           paid_currency: paymentIntent.currency,
-  //           raw_status: paymentIntent.status,
-  //         });
-  //         break;
-  //       case 'payment_intent.payment_failed':
-  //         const failedPaymentIntent = event.data.object;
-  //         const failedMetadata = failedPaymentIntent.metadata;
+      const paymentIntent = await StripePayment.createPaymentIntent({
+        customer_id: customer.billing_id,
+        amount: totalAmount,
+        currency: 'usd',
+        metadata: {
+          order_id: orderId,
+          buyer_id: buyer_id,
+          seller_id: seller_id,
+          total_pay: totalAmount,
+        },
+      });
 
-  //         // here update order status to cancelled and payment status to failed
-  //         await this.prisma.order.update({
-  //           where: { id: failedMetadata.order_id },
-  //           data: {
-  //             payment_status: PaymentStatus.FAILED,
-  //             order_status: OrderStatus.CANCELLED,
-  //           },
-  //         });
+       // 🟢 4. Save initial transaction
+      await this.prisma.paymentTransaction.create({
+        data: {
+          user_id: buyer_id,
+          order_id: orderId,
+          provider: 'stripe',
+          reference_number: paymentIntent.id,
+          amount: totalAmount,
+          currency: 'usd',
+          status: 'pending',
+        },
+      });
 
-  //         // Update transaction status in database
-  //         await TransactionRepository.updateTransaction({
-  //           reference_number: failedPaymentIntent.id,
-  //           status: 'failed',
-  //           raw_status: failedPaymentIntent.status,
-  //         });
-  //       case 'payment_intent.canceled':
-  //         const canceledPaymentIntent = event.data.object;
-  //         // Update transaction status in database
-  //         await TransactionRepository.updateTransaction({
-  //           reference_number: canceledPaymentIntent.id,
-  //           status: 'canceled',
-  //           raw_status: canceledPaymentIntent.status,
-  //         });
-  //         break;
-  //       case 'payment_intent.requires_action':
-  //         const requireActionPaymentIntent = event.data.object;
-  //         // Update transaction status in database
-  //         await TransactionRepository.updateTransaction({
-  //           reference_number: requireActionPaymentIntent.id,
-  //           status: 'requires_action',
-  //           raw_status: requireActionPaymentIntent.status,
-  //         });
-  //         break;
-  //       case 'payout.paid':
-  //         const paidPayout = event.data.object;
-  //         console.log(paidPayout);
-  //         break;
-  //       case 'payout.failed':
-  //         const failedPayout = event.data.object;
-  //         console.log(failedPayout);
-  //         break;
-  //       default:
-  //         console.log(`Unhandled event type ${event.type}`);
-  //     }
+     console.log('PaymentIntent Created:', paymentIntent.client_secret);
+     console.log('Metadata:', paymentIntent.metadata);
 
-  //     return { received: true };
-  //   } catch (error) {
-  //     console.error('Webhook error', error);
-  //     return { received: false };
-  //   }
-  // }
+
+    return {        
+        success: true,
+        message: 'PaymentIntent created successfully',
+        clientSecret: paymentIntent.client_secret,
+        order_id: orderId,
+        totalAmount: totalAmount,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        message: `Error placing order : ${error.message}`,
+      };
+    }
+  }
+
+  @Post('webhook')
+  async handleWebhook(
+    @Headers('stripe-signature') signature: string,
+    @Req() req: Request,
+  ) {
+    try {
+      const payload = req.rawBody.toString();
+      const event = await this.stripeService.handleWebhook(payload, signature);
+
+      // Handle events
+      switch (event.type) {
+        case 'customer.created':
+          break;
+        case 'payment_intent.created':
+          break;
+        case 'payment_intent.succeeded':
+          const paymentIntent = event.data.object;
+          const successMetadata = paymentIntent.metadata;
+          // create tax transaction
+          // await StripePayment.createTaxTransaction(
+          //   paymentIntent.metadata['tax_calculation'],
+          // );
+          // Update transaction status in database
+
+          // here update order status to paid
+          await this.prisma.order.update({
+            where: { id: successMetadata.order_id },
+            data: { payment_status: PaymentStatus.PAID },
+          });
+
+          await TransactionRepository.updateTransaction({
+            reference_number: paymentIntent.id,
+            status: 'succeeded',
+            paid_amount: paymentIntent.amount / 100, // amount in dollars
+            paid_currency: paymentIntent.currency,
+            raw_status: paymentIntent.status,
+          });
+          break;
+        case 'payment_intent.payment_failed':
+          const failedPaymentIntent = event.data.object;
+          const failedMetadata = failedPaymentIntent.metadata;
+
+          // here update order status to cancelled and payment status to failed
+          await this.prisma.order.update({
+            where: { id: failedMetadata.order_id },
+            data: {
+              payment_status: PaymentStatus.FAILED,
+              order_status: OrderStatus.CANCELLED,
+            },
+          });
+
+          // Update transaction status in database
+          await TransactionRepository.updateTransaction({
+            reference_number: failedPaymentIntent.id,
+            status: 'failed',
+            raw_status: failedPaymentIntent.status,
+          });
+        case 'payment_intent.canceled':
+          const canceledPaymentIntent = event.data.object;
+          // Update transaction status in database
+          await TransactionRepository.updateTransaction({
+            reference_number: canceledPaymentIntent.id,
+            status: 'canceled',
+            raw_status: canceledPaymentIntent.status,
+          });
+          break;
+        case 'payment_intent.requires_action':
+          const requireActionPaymentIntent = event.data.object;
+          // Update transaction status in database
+          await TransactionRepository.updateTransaction({
+            reference_number: requireActionPaymentIntent.id,
+            status: 'requires_action',
+            raw_status: requireActionPaymentIntent.status,
+          });
+          break;
+        case 'payout.paid':
+          const paidPayout = event.data.object;
+          console.log(paidPayout);
+          break;
+        case 'payout.failed':
+          const failedPayout = event.data.object;
+          console.log(failedPayout);
+          break;
+        default:
+          console.log(`Unhandled event type ${event.type}`);
+      }
+
+      return { received: true };
+    } catch (error) {
+      console.error('Webhook error', error);
+      return { received: false };
+    }
+  }
 }
